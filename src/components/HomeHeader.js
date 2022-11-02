a7c66750659f00325146efedd5bcb5812c7f2a80
balazs.varga <balazs.varga@profumera.de>
2022-11-02 15:58:56 +0100

    add favicon

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -5,9 +5,6 @@ const HomeHeader = () => {
                 <div className="container d-flex align-items-center justify-content-center text-center home_page_title_container">
                     <div className="row">
                         <div className="col-12">
-
-                        </div>
-                        <div className="col-12">
                             <div className="header-caption">
                                 Pole Dance & Aerial Fitness
                             </div>
