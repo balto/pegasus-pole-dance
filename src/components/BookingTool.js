abbfd9b5e88aec596af66ab4e20e91127068c594
balazs.varga <balazs.varga@profumera.de>
2022-11-02 16:26:16 +0100

    change headers

--- src/components/BookingTool.js
+++ src/components/BookingTool.js
@@ -1,6 +1,7 @@
 const BookingTool = () => {
     return (
         <iframe
+            title="Fitogram"
             frameBorder="0"
             scrolling="yes"
             src="https://widget.fitogram.pro/pegasus-pole-studio?fito-embedded=true&w=/calendar"
