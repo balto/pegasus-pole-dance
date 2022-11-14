2e1b2386d3fe5b66503bf12371d63bffe57d7a2a
balazs.varga <balazs.varga@profumera.de>
2022-11-14 21:31:54 +0100

    add impressum and privacy policy

--- src/components/Impressum.js
+++ src/components/Impressum.js
@@ -2,31 +2,32 @@ const Impressum = () => {
     return (
         <section>
             <div className="container mt-5">
-                <h2>Diensteanbieter</h2>
+                <h2 id="m46">Diensteanbieter</h2>
                 <p>Veronika Varga-Farkas </p>
                 <p>Pegasus Pole (Einzelunternehmen)</p>
                 <p>Schurzstr. 3</p>
                 <p>54292 Trier</p>
                 <p>Deutschland</p>
-                <h2>Kontaktm&ouml;glichkeiten</h2>E-Mail-Adresse: <p><a href="mailto:info@pegasuspole.de">info@pegasuspole.de</a></p>
+                <h2 id="m56">Kontaktm&ouml;glichkeiten</h2>E-Mail-Adresse: <p><a href="mailto:info@pegasuspole.de">info@pegasuspole.de</a></p>
                 Telefon: <p>0175 4454264</p>
                 Weitere Kontaktm&ouml;glichkeiten: <p>Facebook Messenger, WhatsApp Messenger</p>
-                <h2>Audiovisuelle Mediendienste</h2><p>Sitzland: Deutschland</p>
+                <h2 id="m3589">Audiovisuelle Mediendienste</h2><p>Sitzland: Deutschland</p>
                 <p>Zust&auml;ndige Regulierungs-, bzw. Aufsichtsbeh&ouml;rde: Medienanstalt Rheinland-Pfalz, <a href="https://www.lmk-online.de/" target="_blank" rel="noreferrer">https://www.lmk-online.de/</a></p>
-                <h2>Angaben zum Unternehmen</h2>Umsatzsteuer Identifikationsnummer (USt-ID): <p>DE354753696</p>
+                <h2 id="m58">Angaben zum Unternehmen</h2>Umsatzsteuer Identifikationsnummer (USt-ID): <p>DE354753696</p>
                 AGB: <p><a href="https://pegasuspole.de/allgemeine-geschaeftsbedingungen" target="_blank" rel="noreferrer">https://pegasuspole.de/allgemeine-geschaeftsbedingungen</a></p>
-                <h2>Kammer</h2>Kammer (oder vergleichbare berufsrechtlicher Organisation): <p>Industrie- und Handelskammer Trier<br/>Herzogenbuscher Stra&szlig;e 12<br/>54292 Trier</p>
+                <h2 id="m162">Kammer</h2>Kammer (oder vergleichbare berufsrechtlicher Organisation): <p>Industrie- und Handelskammer Trier<br/>Herzogenbuscher Stra&szlig;e 12<br/>54292 Trier</p>
                 Website und Kontakt der Kammer: <p><a href="https://www.ihk-trier.de/" target="_blank" rel="noreferrer">https://www.ihk-trier.de/</a></p>
-                <h2>Online-Streitbeilegung (OS)</h2><p>Online-Streitbeilegung: Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit, die Sie unter <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noreferrer">https://ec.europa.eu/consumers/odr/</a> finden. Verbraucher haben die M&ouml;glichkeit, diese Plattform f&uuml;r die Beilegung ihrer Streitigkeiten zu nutzen. </p>
-                <h2>Social Media und andere Onlinepr&auml;senzen</h2>Dieses Impressum gilt auch f&uuml;r die folgenden Social-Media-Pr&auml;senzen und Onlineprofile: <p><a href="https://www.facebook.com/pegasuspoletrier" target="_blank" rel="noreferrer">https://www.facebook.com/pegasuspoletrier</a></p>
+                <h2 id="m174">Online-Streitbeilegung (OS)</h2><p>Online-Streitbeilegung: Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit, die Sie unter <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noreferrer">https://ec.europa.eu/consumers/odr/</a> finden. Verbraucher haben die M&ouml;glichkeit, diese Plattform f&uuml;r die Beilegung ihrer Streitigkeiten zu nutzen. </p>
+                <h2 id="m172">Social Media und andere Onlinepr&auml;senzen</h2>Dieses Impressum gilt auch f&uuml;r die folgenden Social-Media-Pr&auml;senzen und Onlineprofile: <p><a href="https://www.facebook.com/pegasuspoletrier" target="_blank" rel="noreferrer">https://www.facebook.com/pegasuspoletrier</a></p>
                 <p><a href="https://www.instagram.com/pegasuspoletrier" target="_blank" rel="noreferrer">https://www.instagram.com/pegasuspoletrier</a></p>
                 <p><a href="https://www.tiktok.com/@pegasuspoletrier" target="_blank" rel="noreferrer">https://www.tiktok.com/@pegasuspoletrier</a></p>
-                <h2>Haftungs- und Schutzrechtshinweise</h2><p>Haftungsausschluss: Die Inhalte dieses Onlineangebotes wurden sorgf&auml;ltig und nach unserem aktuellen Kenntnisstand erstellt, dienen jedoch nur der Information und entfalten keine rechtlich bindende Wirkung, sofern es sich nicht um gesetzlich verpflichtende Informationen (z.B. das Impressum, die Datenschutzerkl&auml;rung, AGB oder verpflichtende Belehrungen von Verbrauchern) handelt. Wir behalten uns vor, die Inhalte vollst&auml;ndig oder teilweise zu &auml;ndern oder zu l&ouml;schen, soweit vertragliche Verpflichtungen unber&uuml;hrt bleiben. Alle Angebote sind freibleibend und unverbindlich. </p>
+                <h2 id="m65">Haftungs- und Schutzrechtshinweise</h2><p>Haftungsausschluss: Die Inhalte dieses Onlineangebotes wurden sorgf&auml;ltig und nach unserem aktuellen Kenntnisstand erstellt, dienen jedoch nur der Information und entfalten keine rechtlich bindende Wirkung, sofern es sich nicht um gesetzlich verpflichtende Informationen (z.B. das Impressum, die Datenschutzerkl&auml;rung, AGB oder verpflichtende Belehrungen von Verbrauchern) handelt. Wir behalten uns vor, die Inhalte vollst&auml;ndig oder teilweise zu &auml;ndern oder zu l&ouml;schen, soweit vertragliche Verpflichtungen unber&uuml;hrt bleiben. Alle Angebote sind freibleibend und unverbindlich. </p>
                 <p>Links auf fremde Webseiten: Die Inhalte fremder Webseiten, auf die wir direkt oder indirekt verweisen, liegen au&szlig;erhalb unseres Verantwortungsbereiches und wir machen sie uns nicht zu Eigen. F&uuml;r alle Inhalte und Nachteile, die aus der Nutzung der in den verlinkten Webseiten aufrufbaren Informationen entstehen, &uuml;bernehmen wir keine Verantwortung.</p>
                 <p>Urheberrechte und Markenrechte: Alle auf dieser Website dargestellten Inhalte, wie Texte, Fotografien, Grafiken, Marken und Warenzeichen sind durch die jeweiligen Schutzrechte (Urheberrechte, Markenrechte) gesch&uuml;tzt. Die Verwendung, Vervielf&auml;ltigung usw. unterliegen unseren Rechten oder den Rechten der jeweiligen Urheber bzw. Rechteinhaber.</p>
                 <p>Hinweise auf Rechtsverst&ouml;&szlig;e: Sollten Sie innerhalb unseres Internetauftritts Rechtsverst&ouml;&szlig;e bemerken, bitten wir Sie uns auf diese hinzuweisen. Wir werden rechtswidrige Inhalte und Links nach Kenntnisnahme unverz&uuml;glich entfernen.</p>
-                <h2>Bildnachweise</h2>Bildquellen und Urheberrechtshinweise: <p>Vizoli Photography</p>
+                <h2 id="m169">Bildnachweise</h2>Bildquellen und Urheberrechtshinweise: <p>Vizoli Photography</p>
                 <p><a href="https://www.facebook.com/vizoliphotography" target="_blank" rel="noreferrer">https://www.facebook.com/vizoliphotography</a></p>
+
             </div>
         </section>
     )
