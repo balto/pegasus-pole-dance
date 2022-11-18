7b21384d8b960747d606f2fb19ac121542f50976
balazs.varga <balazs.varga@profumera.de>
2022-11-18 21:07:02 +0100

    add agb

--- src/components/TermsAndConditions.js
+++ src/components/TermsAndConditions.js
@@ -2,21 +2,120 @@ const TermsAndConditions = () => {
     return (
         <section>
             <div className="container mt-5">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent venenatis est imperdiet bibendum dictum. Quisque arcu magna, fermentum in fringilla in, tempus sit amet ligula. Nunc diam quam, fermentum auctor vulputate vitae, commodo eget massa. Vivamus nec elementum risus. Cras at mauris tincidunt, mollis lorem vitae, varius turpis. Nam metus turpis, ornare ac feugiat nec, consectetur ut est. Duis ultrices elementum interdum.
-                </p>
-                <p>
-                    In hendrerit justo a nibh porta scelerisque. Duis bibendum ipsum quis mi tempor facilisis. Curabitur vitae vulputate ipsum. Maecenas non volutpat neque. Vestibulum sed tincidunt magna, in sollicitudin lectus. In eu dui ex. Aenean hendrerit ante nec nisl consectetur posuere. Suspendisse in massa tristique lorem lobortis sagittis vitae maximus ante. Vestibulum pharetra et leo eu euismod. In placerat, elit id maximus bibendum, libero est mattis orci, vel pharetra orci orci vitae quam. Morbi vel nulla a sem pharetra lobortis quis ut velit. In aliquet, mi sit amet venenatis malesuada, mi lacus hendrerit nisi, et feugiat sem justo nec arcu. Aliquam dictum vulputate tellus, in gravida ex posuere at. Proin fringilla arcu dui. Vivamus cursus leo ut dolor condimentum luctus. Curabitur et elit scelerisque urna euismod pharetra nec et ex.
-                </p>
-                <p>
-                    Curabitur non lorem magna. Curabitur varius blandit tortor, quis eleifend velit mollis in. Donec ullamcorper scelerisque sem, sit amet rhoncus nulla lacinia nec. Praesent faucibus purus nec tellus rutrum feugiat. Donec sed gravida quam. Pellentesque lacinia, felis a auctor facilisis, tortor nibh dapibus erat, eget iaculis ipsum erat a ligula. Nulla ullamcorper ullamcorper hendrerit. Vestibulum pretium diam pretium, rutrum quam eget, porttitor nisi. Suspendisse nibh metus, vulputate at fringilla eu, commodo vel tellus. Sed non ultricies leo. Cras eget tellus ut diam molestie ornare ut eget arcu.
-                </p>
-                <p>
-                    Sed eget dignissim neque. Vestibulum pharetra elit non leo euismod malesuada non non lectus. Praesent mauris turpis, dignissim ut quam a, tincidunt fermentum eros. Suspendisse sodales lacus ligula, in consectetur mi suscipit at. Praesent tristique ornare pretium. Pellentesque non massa id nisi tempor blandit. Vestibulum aliquam id justo nec vulputate. Nulla congue faucibus dictum. Maecenas in libero tincidunt, congue odio in, tempus neque. Suspendisse potenti. Cras orci tellus, iaculis et est quis, iaculis rhoncus mi. Cras sodales convallis ligula. Donec id euismod mi, at placerat lectus. Ut sed ex lacinia, feugiat purus in, imperdiet augue.
-                </p>
-                <p>
-                    Suspendisse ut tortor mi. Vestibulum porttitor eros eget est vestibulum, non tempor magna sodales. Quisque at augue convallis, volutpat nulla ac, ornare risus. Pellentesque elementum neque vel sollicitudin ultricies. Nam rutrum libero vestibulum, posuere elit eget, lobortis velit. Quisque tempor, dui vitae sollicitudin lobortis, ante dui auctor lectus, vitae consequat purus sem ultrices nunc. Pellentesque vel dolor sodales, pellentesque massa at, lobortis quam. Aenean id pellentesque orci, ut mattis erat.
-                </p>
+                <h2>§ 1 Allgemeines</h2>
+                <ol>
+                    <li>
+                        Wir bieten Tanz- und Fitness-Kurse in unserem Studio, gelegen in der Luxemburger Straße 148, 54294 Trier, an.
+                    </li>
+                    <li>
+                        Diese Allgemeinen Geschäftsbedingungen (nachstehend <strong>„AGB“</strong>) gelten für das gesamte Rechtsverhältnis zwischen Pegasus Pole (nachstehend <strong>„wir“</strong> bzw. <strong>„uns“</strong>) und dem Kunden (nachstehend <strong>„Kunde“</strong>) im Zusammenhang mit unseren in § 1 (1) genannten Leistungen.
+                    </li>
+                    <li>
+                        Wir widersprechen der Geltung abweichender und/oder über diese Geschäftsbedingungen hinausgehenden Geschäftsbedingungen des Kunden hiermit ausdrücklich.
+                    </li>
+                    <li>
+                        Unser Angebot richtet sich gleichermaßen an Verbraucher und Unternehmer. Für Zwecke dieser Allgemeinen Geschäftsbedingungen, (i) ist ein Verbraucher jede natürliche Person, die den Vertrag zu einem Zweck abschließt, der weder ihrer gewerblichen noch ihrer selbständigen beruflichen Tätigkeit zugerechnet werden kann (§ 13 des Bürgerlichen Gesetzbuches – BGB) und (ii) ist ein „Unternehmer“ eine natürliche oder juristische Person oder eine rechtsfähige Personengesellschaft, die bei Abschluss des Vertrages in Ausübung ihrer gewerblichen oder selbständigen beruflichen Tätigkeit handelt (§ 14 Abs. 1 BGB).
+                    </li>
+                </ol>
+                <h2>§ 2 Vertragsabschluss</h2>
+                <ol>
+                    <li>
+                        Der Vertragsschluss erfolgt auf unserer Website unter Zuhilfenahme desexternen Dienstleisters FitogramPro. Über den Button „jetzt kaufen“ gibt der Kunde einen verbindlichen Antrag zum Erwerb der entsprechenden Leistungen ab.
+                    </li>
+                    <li>
+                        Der Anbieter schickt daraufhin dem Kunden eine automatische Empfangsbestätigung per E-Mail zu, in welcher die Bestellung des Kunden nochmals aufgeführt wird und die der Kunde über die Funktion „Drucken“ ausdrucken kann. Die automatische Empfangsbestätigung dokumentiert lediglich, dass die Bestellung des Kunden bei uns eingegangen ist und stellt keine Annahme des Antrags dar. Der Vertrag kommt erst durch die Bestätigung per E-Mail, dass die Buchung bei uns eingetragen ist, zustande.
+                    </li>
+                </ol>
+                <h2>§ 3 Verfügbarkeit/Änderungen</h2>
+                <ol>
+                    <li>
+                        Die Zeiten der Kurse ergeben sich aus der Beschreibung auf unserer Website.
+                    </li>
+                    <li>
+                        Sofern für einen Kurs eine Mindesteilnehmerzahl erforderlich ist, wird dies ausdrücklich vermerkt. Wir behalten uns vor, aus wichtigem Grund (z.B. Nichterreichen der Mindestteilnehmerzahl) Veranstaltungen auf ein anderes Datum zu verlegen oder abzusagen. Nach Wahl des Kunden werden wir diesem in letzterem Fall einen Ersatztermin anbieten oder die Kursgebühr erstatten.
+                    </li>
+                </ol>
+                <h2>§ 4 Zahlungsmodalitäten</h2>
+                <ol>
+                    <li>
+                        Die Bezahlung erfolgt über unsere Website durch Verwendung des externen Dienstleisters FitogramPro. Als Zahlungsmethoden werden PayPal, Kreditkartenzahlung oder Zahlung per SEPA-Lastschrift akzeptiert.
+                    </li>
+                </ol>
+                <h2>§ 5 Haftung</h2>
+                <ol>
+                    <li>
+                        Vorbehaltlich der in § 6 Abs. 2 genannten Fälle und soweit nicht anders individuell, vereinbart, ist unsere Haftung auf Schadens- oder Aufwendungsersatz, gleich aus welchem Rechtsgrund, ausgeschlossen.
+                    </li>
+                    <li>
+                        wir haften im Rahmen der allgemein gültigen Sorgfaltspflichten für die ordnungsgemäße Durchführung der vereinbarten Leistung entsprechend der gesetzlichen Vorschriften
+                        <ul>
+                            <li>
+                                für Vorsatz oder grobe Fahrlässigkeit,
+                            </li>
+                            <li>
+                                aus einer von uns übernommenen Garantie oder eines übernommenen Beschaffungsrisikos,
+                            </li>
+                            <li>
+                                für die schuldhafte Verletzung von Leben, Körper oder Gesundheit.
+                            </li>
+                        </ul>
+                        Des Weiteren haften wir für die schuldhafte Verletzung einer wesentlichen Vertragspflicht, deren Erfüllung die ordnungsgemäße Durchführung des Vertrags überhaupt möglich macht und auf deren Einhaltung der Vertragspartner regelmäßig vertrauen darf (Kardinalspflicht), im Falle einfacher (leichter) Fahrlässigkeit jedoch begrenzt auf den im Zeitpunkt des Vertragsschlusses vernünftigerweise zu erwartenden und vorhersehbaren Schaden.
+                    </li>
+                    <li>
+                        Die vorstehenden Regelungen in Abs. 1 und 2 gelten sinngemäß auch zugunsten unserer Erfüllungsgehilfen, gesetzlichen Vertreter, Beauftragten, Angestellten und Arbeitnehmer für den Umfang ihrer persönlichen Haftung.
+                    </li>
+                </ol>
+                <h2>§ 6 Ausschluss einzelner Kursteilnehmer</h2>
+                <ol>
+                    <li>
+                        Wir behalten uns vor, einzelne Kursteilnehmer aus wichtigem Grund vom Unterricht auszuschließen. Als wichtige Gründe gelten insbesondere dauerndes, nachhaltiges Stören anderer Kursteilnehmer oder des Lehrpersonals oder ohne unsere ausdrückliche Zustimmung erfolgte Foto-, Video oder Tonaufnahmen.
+                    </li>
+                </ol>
+                <h2>§ 7 Stornierung</h2>
+                <ol>
+                    <li>
+                        Der Kunde kann bis 24 Stunden vor Beginn eines Kurses vom Vertrag durch E-Mail an: info@pegasuspole.de zurücktreten. Maßgeblicher Zeitpunkt ist der Zugang der Erklärung bei uns. Wir erstatten dem Kunden die gezahlte Gebühr im Anschluss innerhalb von 14 Tagen nach Zugang der Rücktrittserklärung.
+                    </li>
+                    <li>
+                        Durch vorstehende Regelungen wird das gesetzliche Widerrufsrecht nicht berührt, dieses besteht unabhängig von den obigen Regelungen.
+                    </li>
+                </ol>
+                <h2>§ 8 Widerrufsrecht</h2>
+                <ol>
+                    <li>
+                        Verbraucher haben bei Abschluss eines Fernabsatzgeschäfts grundsätzlich ein gesetzliches Widerrufsrecht, über das der Anbieter nach Maßgabe des gesetzlichen Musters in seiner Widerrufsbelehrung informiert. Dort findet sich auch ein Muster-Widerrufsformular.
+                    </li>
+                </ol>
+                <h2>§ 9 Urheberrecht</h2>
+                <ol>
+                    <li>
+                        Der Kunde darf unsere Urheberrechte nicht verletzen. Insbesondere im Kurs erlernte Choreografien dürfen ohne Zustimmung des Urhebers weder entgeltlich noch unentgeltlich Dritten aufgeführt oder beigebracht werden. Alle Texte, Beschreibungen, Bilder, Videos sowie das Design unterliegen dem Schutz des Urheberrechtes und anderer Schutzgesetze. Der Schutz der Urheberrechte ist zeitlich unbegrenzt.
+                    </li>
+                    <li>
+                        Im Falle der Nichteinhaltung kann Schadensersatz verlangt werden.
+                    </li>
+                    <li>
+                        Foto-, Video- oder Tonaufnahmen sind nur mit ausdrücklicher Genehmigung von Pegasus Pole gestattet und dürfen nur für private Zwecke verwendet werden. Es ist untersagt, dass der Kunde und das von uns angewendete spezielle Trainingskonzept verbreitet. Es ist ferner untersagt, diese Tanzkurse und deren Inhalt in welcher Form auch immer ohne eine Lizenz von uns an Dritte unmittelbar oder mittelbar weiterzugeben, das Erlernte Dritten zugänglich zu machen oder insbesondere Dritte zu unterrichten.
+                    </li>
+                    <li>
+                        Anfragen über das Unterrichten von anderen Personen in jeglicher Form für ein anderes Institut, Theater, Schule, Verein, eine freie Gruppe, eine Einzelperson oder ähnliches außerhalb unseres regulären Angebots müssen schriftlich (die Form des §126b reicht aus) an uns gerichtet werden.
+                    </li>
+                </ol>
+                <h2>§ 10 Datenschutz</h2>
+                <ol>
+                    <li>
+                        Wir beachten die geltenden datenschutzrechlichen Bestimmungen. Nähere Informationen über die Datenverarbeitung und die Ihnen zustehenden Rechte (Recht auf Auskunft, Recht auf Berichtigung oder Löschung, Recht auf Einschränkung der Verarbeitung, Recht auf Widerspruch gegen die Verarbeitung, Recht auf Datenübertragbarkeit) finden Sie in unserer Datenschutzerklärung, die wir Ihnen auf unserer Website zur Verfügung stellen.
+                    </li>
+                </ol>
+                <h2>§ 11 Anwendbares Recht und Gerichtsstand</h2>
+                <ol>
+                    <li>
+                        Der zwischen uns und dem Kunden bestehende Vertrag unterliegt dem Recht der Bundesrepublik Deutschland unter Ausschluss des UN-Kaufrechts. Die gesetzlichen Vorschriften zur Beschränkung der Rechtswahl und zur Anwendbarkeit zwingender Vorschriften insbesondere des Staates, in dem der Kunde als Verbraucher seinen gewöhnlichen Aufenthalt hat, bleiben unberührt.
+                    </li>
+                    <li>
+                        Ist der Kunde Kaufmann im Sinne des § 1 Abs. 1 des Handelsgesetzbuches (HGB), so sind die für Trier zuständigen Gerichte für alle Streitigkeiten aus oder im Zusammenhang mit dem betreffenden Vertragsverhältnis ausschließlich zuständig. In allen anderen Fällen können wir oder der Kunde Klage vor jedem aufgrund gesetzlicher Vorschriften zuständigen Gericht erheben.
+                    </li>
+                </ol>
             </div>
         </section>
     )
